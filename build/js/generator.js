0d3e9c3f398943a4d775e2e899136397211adfbb
maxgironcodes <maxgironcodes@icloud.com>
2017-09-25 14:52:58 -0500

    Keep matchString as is until I can patch social icons

--- build/js/generator.js
+++ build/js/generator.js
@@ -211,7 +211,7 @@ var generator = (function () {
       return blankInputs;
     },
     matchString: function(search, find) {
-      search.toLowerCase();
+      // search.toLowerCase();
       return search.includes(find);
     },
     readFile: function(input, callback) {
