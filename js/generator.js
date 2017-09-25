8e0d88b485bd964b220f9e0f590f1feb92ce4850
maxgironcodes <maxgironcodes@icloud.com>
2017-09-25 14:46:27 -0500

    Revert "Canvas edits are now saved rather than re-rendering multiple times"
    This reverts commit 0b34cf742708416267e8cea68b72737c264b6503.

--- js/generator.js
+++ js/generator.js
@@ -3,7 +3,6 @@ var canvas = document.querySelector(".generator__canvas");
 
 // Begin generator module
 var generator = (function () {
-  var canvasCopy = document.createElement("canvas"); // Used to save canvas edits throughout app
 
   var settings = {
     width: canvas.width,
@@ -14,9 +13,6 @@ var generator = (function () {
     group: "No group set.", // Store a group of elements for later use
     image: "No image set."
   };
-
-  canvasCopy.width = canvas.width;
-  canvasCopy.height = canvas.height;
 
   var actions = {
     addBottomGradient: function(ratio) {
@@ -43,13 +39,9 @@ var generator = (function () {
           rectOpacity = 0.5,
           padding = { top: 14, right: 70, bottom: 35, left: 35 };
 
-      console.log("Courtesy is: " + courtesy);
-
       if (courtesy.length > 0) {
-        if (settings.image != "No image set.") {
-          actions.clearCanvas();
-          actions.restoreState(); // Restores initial portrait without a courtesy
-        }
+        actions.clearCanvas();
+        actions.renderPortrait();
         actions.drawTextWithRect(courtesy, fSize, fWeight, startX, startY, rectColor, rectOpacity, padding);
       }
     },
@@ -58,25 +50,20 @@ var generator = (function () {
           newHeight = actions.getLowerThirdsHeight(),
           newWidth = Math.round(newHeight * ratio),
           startX = 80,
-          startY = Number(settings.group[0].dataset.baselineY - settings.group[0].dataset.fontSize) + 8;
+          startY = Number(settings.group[0].dataset.baselineY - settings.group[0].dataset.fontSize) + 8,
+          marginRight = 35;
 
-      actions.clearCanvas();
-      actions.addBottomGradient(1/3);
-      actions.fitImage(settings.image, newWidth, newHeight, startX, startY);
-      settings.image.width = newWidth;
-      settings.image.height = newHeight;
-      actions.saveState();
-      console.log("Phoner dimensions are: " + newWidth + ", " + newHeight);
+      if (newHeight) {
+        actions.clearCanvas();
+        actions.addBottomGradient(1/3);
+        actions.fitImage(settings.image, newWidth, newHeight, startX, startY);
+        console.log("Phoner dimensions are: " + newWidth + ", " + newHeight);
+        startX += newWidth + marginRight;
+        actions.renderLowerThirds(startX);
+      }
     },
     clearCanvas: function() {
       settings.context.clearRect(0, 0, settings.width, settings.height);
-    },
-    clearInputs: function() {
-      var allInputs = document.querySelectorAll("input");
-
-      allInputs.forEach(function(input) {
-        input.value = "";
-      });
     },
     downloadCanvas: function() {
       canvas.toBlob(function(blob) {
@@ -138,7 +125,9 @@ var generator = (function () {
       var rectWidth = context.measureText(text).width;
       var rectHeight = font.size;
 
-      if (rectOpacity > 0) context.globalAlpha = 0.5;
+      if (rectOpacity > 0) {
+        context.globalAlpha = 0.5;
+      }
 
       context.fillRect(startX, startY, (rectWidth + padding.right), (rectHeight + padding.bottom));
       context.globalAlpha = 1.0; // Reset opacity for future drawings
@@ -187,28 +176,26 @@ var generator = (function () {
     },
     getLowerThirdsHeight: function() {
       var inputsArray = settings.group,
+          blankInputs = 0,
           totalHeight = 0;
 
       inputsArray.forEach(function(input) {
         var fSize = Number(input.dataset.fontSize),
             baselineY = Number(input.dataset.baselineY);
 
-        totalHeight += fSize;
-      });
-
-      return totalHeight;
-    },
-    getLowerThirdsBlanks: function() {
-      var inputsArray = settings.group,
-          blankInputs = 0;
-
-      inputsArray.forEach(function(input) {
-        if (!input.value) {
+        if (input.value) {
+          totalHeight += fSize;
+        } else {
           blankInputs++;
         }
       });
 
-      return blankInputs;
+      if (blankInputs === 0) {
+        return totalHeight;
+      } else {
+        alert(blankInputs + " text field(s) were left empty. You must fill each of them to submit.");
+        return false;
+      }
     },
     matchString: function(search, find) {
       search.toLowerCase();
@@ -218,7 +205,7 @@ var generator = (function () {
       var file = input.files[0],
           reader = new FileReader(),
           image = new Image(),
-          display = input.parentElement.querySelector("span");
+          span = input.nextSibling; // Assume input.js-get-file has span sibling to display file name
 
       if (file) {
         reader.onload = function(event) {
@@ -226,12 +213,11 @@ var generator = (function () {
           image.src = event.target.result;
           settings.image = image;
         };
-        display.innerHTML = input.value.substring(12); // Remove "C:\fakepath\" from imageURL
+        span.innerHTML = input.value.substring(12); // Remove "C:\fakepath\" from imageURL
         reader.readAsDataURL(event.target.files[0]);
-        actions.clearInputs();
         console.log("User image uploaded successfully.");
+        // return file;
       }
-
       setTimeout(function() {
         callback();
       }, 200);
@@ -247,44 +233,22 @@ var generator = (function () {
       actions.fitImage(image, width, height, 0, 0, 0.5, (offsetY ? offsetY : 0.5)); // Draw background image
       context.filter = "none"; // Reset filters so they don't apply to the next image
       actions.fitImage(image, ((width / 2) - (width / 25)), height, (width / 2), 0); // Fit foreground image against the half-way point of canvas, crop as portrait
-      actions.saveState();
     },
-    renderLowerThirds: function() {
-      var inputsArray = settings.group,
-          blankInputs = actions.getLowerThirdsBlanks(),
-          startX = 80,
-          marginRight = 35;
+    renderLowerThirds: function(startX) {
+      var inputsArray = settings.group;
 
-      if (settings.image != "No image set.") startX += settings.image.width + marginRight;
-
-      if (blankInputs > 1) {
-        alert(blankInputs + " text fields are empty. Please fill them out before submitting.");
-      } else if (blankInputs === 1) {
-        alert(blankInputs + " text field is empty. Please fill it out before submitting.");
-      } else {
-        if (settings.image == "No image set.") {
-          actions.clearCanvas();
-          actions.addBottomGradient(1/3);
-        } else {
-          actions.clearCanvas();
-          actions.restoreState();
-        }
-        inputsArray.forEach(function(input) {
-          inputValue = input.value;
-          fSize = input.dataset.fontSize;
-          fWeight = input.dataset.fontWeight;
-          baselineY = input.dataset.baselineY;
-
-          actions.drawText(inputValue, fSize, fWeight, startX, baselineY);
-        });
+      if (!startX) {
+        startX = 80;
       }
-    },
-    restoreState: function() {
-      settings.context.drawImage(canvasCopy, 0, 0);
-    },
-    saveState: function() {
-      var contextCopy = canvasCopy.getContext("2d");
-      contextCopy.drawImage(canvas, 0, 0);
+
+      inputsArray.forEach(function(input) {
+        inputValue = input.value;
+        fSize = input.dataset.fontSize;
+        fWeight = input.dataset.fontWeight;
+        baselineY = input.dataset.baselineY;
+
+        actions.drawText(inputValue, fSize, fWeight, startX, baselineY);
+      });
     }
   };
 
@@ -295,36 +259,45 @@ var generator = (function () {
       var parent = button.parentElement,
           inputsArray = parent.querySelectorAll("input"),
           input = inputsArray[0],
-          inputClass = input.className;
-
-      if (inputsArray.length > 1) {
-        settings.group = inputsArray;
-      }
+          inputClass = input.className,
+          inputValue = input.value;
 
       button.addEventListener("click", function() {
-
-        switch (true) {
-          case actions.matchString(inputClass, "js-add-courtesy"):
-            actions.addCourtesy(input.value);
-            break;
-          case actions.matchString(inputClass, "js-upload-portrait"):
-            input.click();
-            input.onchange = function() {
-              actions.readFile(input, actions.renderPortrait);
-            };
-            break;
-          case actions.matchString(inputClass, "js-upload-phoner"):
-            input.click();
-            input.onchange = function() {
-              actions.readFile(input, actions.addPhoner);
-            };
-            break;
-          case actions.matchString(inputClass, "js-add-text-group"):
-            actions.renderLowerThirds();
-            break;
-          default:
-            console.log("No match found for inputClass.");
-            break;
+        if (inputsArray.length > 1) {
+          settings.group = inputsArray;
+          actions.renderLowerThirds();
+        } else {
+          switch (true) {
+            case actions.matchString(inputClass, "js-add-courtesy"):
+              if (inputValue) {
+                actions.addCourtesy(inputValue);
+              } else {
+                alert("No text added to courtesy field.");
+              }
+              break;
+            case actions.matchString(inputClass, "js-add-courtesy"):
+              if (inputValue) {
+                actions.addCourtesy(inputValue);
+              } else {
+                alert("No text added to courtesy field.");
+              }
+              break;
+            case actions.matchString(inputClass, "js-upload-portrait"):
+              input.click();
+              input.onchange = function() {
+                actions.readFile(input, actions.renderPortrait);
+              };
+              break;
+            case actions.matchString(inputClass, "js-upload-phoner"):
+              input.click();
+              input.onchange = function() {
+                actions.readFile(input, actions.addPhoner);
+              };
+              break;
+            default:
+              console.log("No match found for inputClass.");
+              break;
+          }
         }
       });
     });
